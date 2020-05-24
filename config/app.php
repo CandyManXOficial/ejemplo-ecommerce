835fd3fc9dfca211f9af548ccb47f698024c082c
Luis Manuel <candymanxgo@gmail.com>
2020-05-24 13:02:13 -0500

    Creamos la vista para agregar un producto, se agregaron valores a los archivos css y se implemento la paqueteria de LaravelCollective

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        *  Providers agregados
+        */
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +231,12 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+        * Aliases agregados
+        */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
