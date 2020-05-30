cfb97c6857fa7ed9648b02351965688f8ed26611
Luis Manuel <candymanxgo@gmail.com>
2020-05-29 22:03:39 -0500

    Se habilita la opción para eliminar un producto así como la protección para que solo aparezca la opción de eliminar cuando exista una sesión de usuario disponible.

--- resources/views/products/delete.blade.php
+++ resources/views/products/delete.blade.php
@@ -1,3 +1,5 @@
-{!! Form::open(['method' => 'DELETE', 'route' => ['productos.destroy', $product->id], 'onsubmit' => 'return confirm("Are you sure want do this?")']) !!}
-<input type="submit" value="Eliminar" class="btn btn-danger">
-{!! Form::close() !!}
+@auth
+    {!! Form::open(['method' => 'DELETE', 'route' => ['productos.destroy', $product->id], 'onsubmit' => 'return confirm("Are you sure want do this?")']) !!}
+        <input type="submit" value="Eliminar" class="btn btn-danger">
+    {!! Form::close() !!}
+@endauth
