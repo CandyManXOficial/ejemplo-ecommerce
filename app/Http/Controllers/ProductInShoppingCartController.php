86bf9104f16542506e720d76b5da5c6bbff3d4d5
Luis Manuel <candymanxgo@gmail.com>
2020-05-31 10:51:00 -0500

    Segunda parte de los archivos del controlador

---
+++ app/Http/Controllers/ProductInShoppingCartController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductInShoppingCartController extends Controller
+{
+    public function store(Request $request){}
+
+    public function destroy($id){}
+}
