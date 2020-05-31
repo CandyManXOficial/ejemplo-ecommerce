78e9ad0aa184414cb57b60139144feba32f0a034
Luis Manuel <candymanxgo@gmail.com>
2020-05-31 10:40:32 -0500

    Se crea la migración y el modelo para almacenar los articulos que serán comprados.

---
+++ database/migrations/2020_05_31_152307_create_product_in_shopping_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductInShoppingCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_in_shopping_carts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('shopping_cart_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_in_shopping_carts');
+    }
+}
